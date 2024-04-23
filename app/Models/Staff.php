445c94109ba510268e51049ca4fa25778928b32e
Mayadi21 <mayadisilalahi@gmail.com>
2024-04-23 12:18:20 +0700

    migrate dan seeder

---
+++ app/Models/Staff.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Staff extends Model
+{
+    use HasFactory;
+
+    protected $table = 'staff';
+    
+    protected $primaryKey = 'id';
+
+    protected $fillable = ['id', 'nip', 'nama', 'email', 'tanggal_lahir', 'no_hp'];
+
+    public $timestamps = 'false';
+}
